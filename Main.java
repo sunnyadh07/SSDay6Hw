0e444920cb74b0a878251f7514b85dfe33bd5b6b
Sunny Adhikari <sunnyadh07@gmail.com>
2021-03-13 23:03:44 -0800

    First commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        user newUser = new user("Robin", "McDonald", "roobin", "strongPW");
+        System.out.println(newUser);
+
+        user oldUser = new user("Ben", "James", "benjames", "benjamin");
+        System.out.println(oldUser);
+
+    }
+}
